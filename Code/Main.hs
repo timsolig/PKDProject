e1f6647fc507e4ae83bef8601a4ca02408d47e7e
Tim Solig <tim.solig@gmail.com>
2021-02-11 15:58:55 +0100

    you deserve a break!

---
+++ Code/Main.hs
@@ -0,0 +1,16 @@
+ import Control.Concurrent
+ 
+main :: IO ()
+main = do
+    putStrLn "Alright, time to work!"
+    loadMsg "...doing some work..." 3
+    putStrLn "You deserve a break!"
+    loadMsg "...having a break..." 300
+    main
+
+loadMsg :: String -> Int -> IO ()
+loadMsg _ 0 = return ()
+loadMsg str x = do
+    threadDelay 1000000
+    putStrLn str
+    loadMsg str (x-1)
