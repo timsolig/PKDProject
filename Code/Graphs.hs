950be0903d464c33ec02bf0f761e8c6fcd7f3273
Tim Solig <tim.solig@gmail.com>
2021-02-16 11:58:59 +0100

    Första försök till datatyper för labytinter

--- Code/Graphs.hs
+++ Code/Graphs.hs
@@ -1,11 +1,27 @@
-import Data.Matrix
-import Numeric.Matrix
-
-type Matrix = [[Int]]
-
-type Maze = [[Bool]]
 
 
-showMatrix:: Matrix -> String
-showMatrix xs = xs
-showMatrix 
+data Cell = Edge | Closed | Path deriving (Show)
+
+type Maze = [[Cell]]
+
+
+createClosedMaze :: Int -> Maze
+createClosedMaze n = foo n n 
+    where 
+        foo :: Int -> Int -> Maze
+        foo x n 
+            | x == -1 = [replicate (n+2) Edge]
+            | x == n = foo (x-1) n ++ [replicate (n+2) Edge] 
+            | otherwise  = foo (x-1) n ++ [Edge : replicate n Closed ++ [Edge]]
+
+
+showMaze:: Maze -> String
+showMaze [[x]] = show x
+showMaze (x:xs) =  show x ++ "\n" ++ showMaze xs
+
+
+
+prim :: Maze -> Maze
+prim m = primAux m []
+
+
