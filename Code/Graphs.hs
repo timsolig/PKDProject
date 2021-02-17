4a7c9239089952c8495a734184d7c180126c20f7
Tim Solig <tim.solig@gmail.com>
2021-02-17 11:50:47 +0100

    Lite mer labyrint

--- Code/Graphs.hs
+++ Code/Graphs.hs
@@ -1,15 +1,11 @@
 
-import qualified Stack as S 
+import qualified Stack as S
 
-import System.Random 
+import System.Random
 
 import System.IO.Unsafe ( unsafeDupablePerformIO )
 
--- data Cell = Cell 
---           | Closed
---           | Open deriving (Eq, Show)
-
- 
+import Data.Set
 type Cell = (Int,Int)
 type Wall = (Cell,Cell)
 
@@ -17,7 +13,12 @@ type Maze = ([Cell],[Wall])
 
 
 
-
+{-pickRandom list
+Something weird with IO monads and shit. And some randomness.
+    RETURNS: Picks a random element from list
+    EXAMPLES:
+        pickRandom [1,2] == 1 || 2
+-}
 pickRandom :: [a] -> a
 pickRandom xs = unsafeDupablePerformIO (fmap (xs !!) $ randomRIO (0, length xs - 1))
 
@@ -38,48 +39,73 @@ pickRandom xs = unsafeDupablePerformIO (fmap (xs !!) $ randomRIO (0, length xs -
 
 
 
-
+{-del element list
+  Deletes an element from a list
+    RETURNS: Every element in list not equal to element 
+-}
 del :: Eq a => a -> [a] -> [a]
-del x lst = filter (/= x) lst
-
+del x lst = Prelude.filter (/= x) lst
 
 
 {-createCells num
-Creates a grid of cells
+  Creates a grid of cells
     PRE: num > 0
     RETURNS: Cartesian coordinates of the (num x num)-grid
+    EXAMPLES:
+        createCells 3 == [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)
     -}
 createCells :: Int -> [Cell]
-createCells n = [(i,j) | i <- [0..n-1], j <- [0..n-1] ]
+createCells n =           [(i,j) | i <- [0..n-1], j <- [0..n-1]]
+
+
+createWalls :: Int -> [Wall]
+createWalls n = [((i,j),(i',j')) | i <- [0..n-1], j <- [0..n-1], i' <- [0..n-1], j' <- [0..n-1], (abs (i-i') == 1  && j == j') || (abs (j-j') == 1 && i == i')]
+
 
 {- prim cells
- Generates a maze basen on the Prim's randomized algorithm
+ Generates a maze basen on Prim's randomized algorithm.
     PRE: length cells > 0 
-    RETURNS: 
+    RETURNS: Pairs from cells which are walls in the generated maze. 
 
 -}
 prim :: [Cell] -> Maze
-prim cells = primAux [] [] cells 
+prim = primAux [] []
     where
         {-Auxiliary function holding the set of paths and walls as well as the original cells-}
         primAux :: [Cell] -> [Wall]-> [Cell] -> Maze
         primAux pathSet wallSet unvisitedCells
-            | null pathSet = let startCell = (0,0) in primAux (startCell : pathSet) wallSet (del startCell unvisitedCells) 
-            
-            | not (null unvisitedCells) = let randomCell = pickRandom (neighbours pathSet unvisitedCells) in case length $ neighbours [randomCell] pathSet of 
-                                            
+            | Prelude.null pathSet = let startCell = (0,0) in primAux (startCell : pathSet) wallSet (del startCell unvisitedCells)
+
+            | not (Prelude.null unvisitedCells) = let randomCell = pickRandom (neighbours pathSet unvisitedCells) in case length $ neighbours [randomCell] pathSet of
+
                                             1 -> primAux (randomCell : pathSet) (wallSet) (del randomCell unvisitedCells)
                                             _ -> primAux (randomCell : pathSet) ((randomCell, pickRandom (neighbours [randomCell] pathSet)) : wallSet) (del randomCell unvisitedCells)
 
-            | otherwise = ([],wallSet)
+            | otherwise = (pathSet,wallSet)
 
 
+
+
+randomizedDFS :: [Cell] -> Maze
+randomizedDFS = undefined
 
 
 {-neighbours cells list
  Gives the unvisitedCells that are neighbouring a specific edge
     RETURNS: unvisitedCells in edgeList that are adjacent to edge
 -}
+neighbours :: [Cell] -> [Cell] -> [Cell]
+neighbours [] _ = []
+neighbours (c:cs) cells = cellNeigbours c cells ++ neighbours cs cells
+-- where
+cellNeigbours :: Cell -> [Cell] -> [Cell]
+cellNeigbours _ [] = []
+cellNeigbours edge@(i,j) edgeList@((i',j'):lst)
+    | i' == i && ( abs (j-j') == 1 )   = (i',j') : cellNeigbours edge lst
+    | j' == j && ( abs (i-i') == 1 )   = (i',j') : cellNeigbours edge lst
+    | otherwise = cellNeigbours edge lst
+
+
 
 {-
              (i-1,j)
@@ -89,13 +115,3 @@ prim cells = primAux [] [] cells
              (i+1,j)
 -}
 
-neighbours :: [Cell] -> [Cell] -> [Cell]
-neighbours [] _ = []
-neighbours (c:cs) cells = cellNeigbours c cells ++ neighbours cs cells
-    where
-        cellNeigbours :: Cell -> [Cell] -> [Cell]
-        cellNeigbours _ [] = []
-        cellNeigbours edge@(i,j) edgeList@((i',j'):lst)
-            | i' == i && ( abs (j-j') == 1 )   = (i',j') : cellNeigbours edge lst
-            | j' == j && ( abs (i-i') == 1 )   = (i',j') : cellNeigbours edge lst
-            | otherwise = cellNeigbours edge lst
