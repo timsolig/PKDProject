8c9bcb07b53663d54089658183a454a4ff0e3916
Jonas Björk <jonas.betual@gmail.com>
2021-02-17 10:47:16 +0100

    viktor fixar inte

--- Code/glossTestJONAS.hs
+++ Code/glossTestJONAS.hs
@@ -1,7 +1,7 @@
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
-<<<<<<< HEAD
+
 import Graphics.Gloss.Interface.Pure.Game
 
 --import Graphics.Gloss.Data
@@ -171,45 +171,3 @@ inputHandler (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = (x + 10, y)
 inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) (x, y) = (x - 10, y)
 inputHandler _ w = w
 -}
-=======
-
-windowWidth = 200
-windowHeight = 200
-x0 = -windowWidth/2
-y0 = -windowHeight/2
-xMax = negate x0
-yMax = negate y0
-
-rows = 5 :: Float
-cols = 5 :: Float
-
-window :: Display
-window = InWindow "Nice Window" (200, 200) (50, 50)
-
-background :: Color
-background = white
-
--- Maze edges
-edges = [
-    Line [(x0, y0),(xMax, y0)],
-    Line [(x0, yMax), (xMax, yMax)],
-    Line [(x0, y0), (x0, yMax)],
-    Line [(xMax, y0), (xMax, yMax)]
-    ]
-
-addColumns :: Float -> Float -> [Picture]
-addColumns num tot
-    | num > 0 = Line [(x0 + num * windowWidth/tot,y0), (x0 + num * windowWidth/tot,yMax)] : addColumns (num-1) tot
-    | otherwise = []
-
-addRows :: Float -> Float -> [Picture]
-addRows num tot
-    | num > 0 = Line [(x0, y0 + num * windowHeight/tot), (xMax, y0 + num * windowHeight/tot)] : addRows (num-1) tot
-    | otherwise = []
-
-drawing :: Picture
-drawing = pictures ((addColumns (cols-1) cols) ++ (addRows (rows-1) rows))
-
-main :: IO ()
-main = display window background drawing
->>>>>>> 139237d7a07bfa053c56a72eb6825f3fb5c896c3
