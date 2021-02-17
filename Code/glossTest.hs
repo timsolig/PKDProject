c3c92b0f3c76d0bd0c16750a4519fb9434d0b2da
Viktor <viktor.hultsten@gmail.com>
2021-02-17 14:05:16 +0100

    gjorde nånting coolt

--- Code/glossTest.hs
+++ Code/glossTest.hs
@@ -1,42 +1,59 @@
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 
-windowWidth = 200
-windowHeight = 200
-x0 = -windowWidth/2
-y0 = -windowHeight/2
-xMax = negate x0
-yMax = negate y0
+--- INSTÄLLNINGAR Start ---
 
-rows = 5 :: Float
-cols = 5 :: Float
+-- Fönsterstorlek
+windowSize = 500
+
+-- Antal rutor
+gridSize = 3
+
+-- Koordinater för väggar
+wallList = [
+    (0,1),
+    (2,0),
+    (2,1),
+    (1,1)
+    ]
+
+--- INSTÄLLNINGAR Slut ---
+
+xMax = windowSize / 2
+x0 = negate xMax
+y0 = windowSize / 2
+yMax = negate y0
+cellSize = windowSize / gridSize
 
 window :: Display
-window = InWindow "Nice Window" (200, 200) (50, 50)
+window = InWindow "A Mazing Game" (round windowSize, round windowSize) (0,0)
 
 background :: Color
 background = white
 
--- Maze edges
-edges = [
-    Line [(x0, y0),(xMax, y0)],
-    Line [(x0, yMax), (xMax, yMax)],
-    Line [(x0, y0), (x0, yMax)],
-    Line [(xMax, y0), (xMax, yMax)]
-    ]
+cellMaker :: Float -> Float -> Picture
+cellMaker x y
+    | not (elem ((x-1), (y-1)) wallList) = Blank
+    | otherwise =
+        Polygon [
+            (x0 + x * cellSize, y0 - y * cellSize),
+            (x0 + x * cellSize, y0 - (y-1) * cellSize),
+            (x0 + (x-1) * cellSize, y0 - (y-1) * cellSize),
+            (x0 + (x-1) * cellSize, y0 - y * cellSize)
+        ]
 
-addColumns :: Float -> Float -> [Picture]
-addColumns num tot
-    | num > 0 = Line [(x0 + num * windowWidth/tot,y0), (x0 + num * windowWidth/tot,yMax)] : addColumns (num-1) tot
-    | otherwise = []
+rowMaker :: Float -> Float -> [Picture]
+rowMaker 0 _ = []
+rowMaker _ 0 = []
+rowMaker x y =
+    cellMaker x y : rowMaker (x-1) y
 
-addRows :: Float -> Float -> [Picture]
-addRows num tot
-    | num > 0 = Line [(x0, y0 + num * windowHeight/tot), (xMax, y0 + num * windowHeight/tot)] : addRows (num-1) tot
-    | otherwise = []
+gridMaker :: Float -> [Picture]
+gridMaker 0 = []
+gridMaker n = rowMaker gridSize n ++ gridMaker (n-1)
 
 drawing :: Picture
-drawing = pictures ((addColumns (cols-1) cols) ++ (addRows (rows-1) rows))
+drawing = pictures (gridMaker gridSize)
 
 main :: IO ()
 main = display window background drawing
