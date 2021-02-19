08d65d27b59ea94348634a58860814f2cc6019b8
Jonas Björk <jonas.betual@gmail.com>
2021-02-19 14:14:57 +0100

    git blame jonas

--- Code/Main.hs
+++ Code/Main.hs
@@ -1,19 +1,10 @@
 import Control.Concurrent
  
-import Graphs as A_mazeing_Stuff  
--- import Glossstuff
+import Move
 
-main :: IO ()
-main = do
-    putStrLn "Alright, time to work!"
-    loadMsg "...doing some work..." 3
-    putStrLn "You deserve a break!"
-    loadMsg "...having a break..." 300
-    main
+import Graphs
 
-loadMsg :: String -> Int -> IO ()
-loadMsg _ 0 = return ()
-loadMsg str x = do
-    threadDelay 1000000
-    putStrLn str
-    loadMsg str (x-1)
+import Clocks
+
+main = do 
+    Move.startGame gameSize
--- Code/Move.hs
+++ Code/Move.hs
@@ -1,123 +1,173 @@
+module Move (startGame ) where
+
+
 import Graphs
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
-{- Skapar en maze med "tunna väggar" -}
+{-PlayerPosition
+  Represents the position of a player
+  Tuple holding the coordinates
+-}
+type PlayerPosition = (Float,Float)
 
-type Cell = (Float,Float)
---- INSTÄLLNINGAR Start ---
+-- '
+-- data GameInfo = Game { size :: Float
+--                       , wallLength = windowSize / gridSize :: Float
+--                       , wallRadius = wallLength / 2 :: Float
+--                       , x0 = (negate xMax) :: Float
+--                       , y0 = (windowSize / 2) :: Float
+--                       , xMax = (windowSize / 2) ::Float
+--                       , yMax = (negate y0) :: Float
+--                       } deriving (Show)   
 
--- Fönsterstorlek
+
+{-GLOBAL VARIABLES-}
 windowSize = 1000  
-
--- Antal rutor
-size :: Float
-size = 20
-
---- INSTÄLLNINGAR Slut ---
-
---Initial world
-type World = (Float,Float)
-initialWorld = drawing  
-
--- gridSize :: Float
+size = 10 :: Float
 gridSize = size
-
--- walls = Graphs.prim cells, koordinater för väggar
-walls = Graphs.iterDFS $ Graphs.createCells size
-
--- till move func
-moveDist = windowSize / gridSize
-startPosFix = moveDist/2
-
-xMax = windowSize / 2
-x0 = negate xMax
-y0 = windowSize / 2
-yMax = negate y0
+initialWorld = drawing  
 
 wallLength = windowSize / gridSize
 wallRadius = wallLength / 2
+xMax = (windowSize / 2)
+x0 = (negate (xMax))
+y0 = (windowSize / 2)
+yMax = (negate y0)
 
-window :: Display   
-window = InWindow "A Mazing Game" (round windowSize, round windowSize) (0,0)
+background = white :: Color
 
-background :: Color
-background = white
 
-createWall :: (Main.Cell, Main.Cell) -> Picture
-createWall ((c1_x, c1_y), (c2_x, c2_y)) =
-    Line [(l1_x, l1_y), (l2_x, l2_y)]
-    where
-        x_mid = x0 + wallLength * max c1_x c2_x
-        y_mid = y0 - wallLength * max c1_y c2_y
-        (l1_x, l2_x, l1_y, l2_y) =
-            if (c1_x == c2_x) then
-                (x_mid, x_mid + wallLength, y_mid, y_mid)
-            else
-                (x_mid, x_mid, y_mid, y_mid - wallLength)
+walls = Graphs.iterDFS $ Graphs.createCells size
 
-createWalls' :: [(Main.Cell, Main.Cell)] -> [Picture]
-createWalls' [] = []
-createWalls' (x:xs) =
-    createWall x : createWalls' xs
 
+{-startGame gameInfo -}
+startGame size = undefined
+
+
+-- till move func
+--moveDist = windowSize / gridSize
+--startPosFix = moveDist/2
+
+
+
+
+{-createWalls walls
+ Creates representation of a walls in a maze
+-}
+createWalls :: Graphs.Maze -> [Picture]
+createWalls [] = []
+createWalls (x:xs) =
+    singleWall x : Move.createWalls xs
+  where
+    {-singleWall wallTuple
+      Creates representation of a single wall
+
+        RETURNS: Picture showing the wall between cells in 'wallTuple'
+        
+    -}
+    singleWall :: Graphs.Wall -> Picture
+    singleWall ((c1_x, c1_y), (c2_x, c2_y)) =
+        Line [(l1_x, l1_y), (l2_x, l2_y)]
+        where
+            x_mid = x0 + wallLength * max c1_x c2_x
+            y_mid = y0 - wallLength * max c1_y c2_y
+            (l1_x, l2_x, l1_y, l2_y) =
+                if (c1_x == c2_x) then
+                    (x_mid, x_mid + wallLength, y_mid, y_mid)
+                else
+                    (x_mid, x_mid, y_mid, y_mid - wallLength)
+
+{-
+converts maze into Picture
+-}
 drawing :: Picture
-drawing = pictures (createWalls' (walls :: [(Main.Cell,Main.Cell)]))
+drawing = pictures (Move.createWalls (walls :: [(Graphs.Cell,Graphs.Cell)]))
 
+--mål shape om en sådan ska användas.
+goal :: Picture
+goal = (color green (circleSolid x))
+  where x = windowSize / (3*gridSize)
 
---                      MOVE
--- =================================================
-
---Hur stor spelaren ska vara  
+{-
+Picture with player shape as a solid circle
+-}
 player:: Picture
 player = (color red (circleSolid x))
   where x = windowSize / (2*gridSize)
 
--- Flyttar player med translate
--- Starta längst upp i vänster hörn = x0 yx
--- Varje steg som tas så ska wallLength gås
+{-movePlayer dx dy
+  Moves the player icon 
+-}
 movePlayer ::Float -> Float -> Picture
-movePlayer x y = Pictures [text cord, translate (x*wallLength+ wallRadius) (y*wallLength-wallRadius){-(x0+x*wallLength + wallRadius) (y0+y*wallLength - wallRadius) -}(player),initialWorld]
-  where cord = show x ++","++ show y
-windowDisplay :: Display
-windowDisplay = InWindow "Testar Gloss" (round windowSize, round windowSize) (10, 10)
+movePlayer x y = Pictures [translate (correctionX x) (correctionY y) (player),initialWorld, goal]
+  where
+    {-correctioX and correctionY moves the playerdot so it is centerd in the cell.-}
+    correctionX x = x0+x*wallLength + wallRadius
+    correctionY y = y0-y*wallLength - wallRadius
 
--- Laddar om frame med samma maze med player på ny eller samma position
-drawPlayfield :: World -> Picture
+
+
+{-drawPlayfield
+  Refreshes a maze with player on a new (or the same) position
+    RETURNS:
+-}
+drawPlayfield :: PlayerPosition -> Picture
 drawPlayfield (x,y) = translate 0 0 $ movePlayer x y
- 
- -- where world = 
- --                     Pictures [color black (line [ (-100, -300), (-100,  300) ]) <>
- --                               color black (line [ ( 100, -300), ( 100,  300) ]) <>
- --                               color black (line [ (-300,  100), ( 300,  100) ]) <>
- --                               color black (line [ (-300, -100), ( 300, -100) ])]
---
---drawingFunc :: World -> Picture
---drawingFunc (x, y) = translate x y (ThickCircle 20 20)
 
-inputHandler :: Event -> World -> World
+
+
+{-inputHandler keyStroke world
+  
+  RETURNS: Updated playfield if keyStroke moves player not through a wall.
+  
+-}
+inputHandler :: Event -> PlayerPosition -> PlayerPosition
 inputHandler (EventKey (SpecialKey key) Down _ _ ) (x,y) = case validMove (x,y) direction of 
                                                                                       True -> direction
                                                                                       _    -> (x,y)
   where direction = case key of 
-                              KeyUp -> (x, y + 1)
-                              KeyDown -> (x, y - 1)
+                              KeyUp -> (x, y - 1)
+                              KeyDown -> (x, y + 1)
                               KeyRight -> (x + 1, y)
                               KeyLeft -> (x - 1, y)
 inputHandler _ (x,y) = (x,y)
 
-validMove :: Main.Cell -> Main.Cell -> Bool
-validMove a b = not $ (a,b) `elem` walls || (b,a) `elem` walls
 
-
-updateFunc :: Float -> World -> World
-updateFunc _ (x, y) = (x, y)
+{-validMove cell1 cell2
   
+    RETURNS: True if there is no wall between cell1 and cell2
+    
+-}
+validMove :: Graphs.Cell -> Graphs.Cell -> Bool
+validMove a b@(x,y) 
+  | x <  0 = False
+  | x >= gridSize = False 
+  | y < 0 = False 
+  | y >= gridSize = False
+  | otherwise = not $ (a,b) `elem` walls || (b,a) `elem` walls 
+  
+
+{-windowDisplay
+  contains info for windowDisplay in play
+-}
+--windowDisplay :: Display
+--windowDisplay = InWindow "A Mazing Game" (200, 200) (100, 500) --(((round windowSize) + 200), ((round windowSize) + 200))
+
+{-
+set color of background
+-}
+
+{-updateFunc
+  No  knows
+-}
+updateFunc :: Float -> PlayerPosition -> PlayerPosition
+updateFunc _ (x, y) = (x, y)
 
 main :: IO ()
 main = play
-  windowDisplay -- size of window
+  FullScreen --windowDisplay -- size of window
   background --color
   30 --fps
   (0,0) --Initial World
