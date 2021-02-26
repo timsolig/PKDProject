02fc1d16818442caf6da6e45bd26076a7583dd75
Tim Solig <tim.solig@gmail.com>
2021-02-26 17:37:06 +0100

    Blame all on Jonas

--- Code/Delivery/PlayGame.hs
+++ Code/Delivery/PlayGame.hs
@@ -2,17 +2,20 @@ import Move
 
 import Render
 
+import GameData
+
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
-{-func 
-  func descirption
-    PRE:
-    RETURNS:
+import Test.HUnit
+
+{-initialState icon1 icon2 
+  Takes two pictures and returns the initial state of a game.
+    RETURNS: The initial state for a game where player icon i "icon1" and goal icon is "icon2".
     EXAMPLES:
 -}
 initialState :: Picture -> Picture -> GameState
-initialState p1 p2= Game {
+initialState playerIcon goalIcon = Game {
         startMenu    = True,
         goalMenu     = False,
         gridSize     = undefined,
@@ -22,14 +25,54 @@ initialState p1 p2= Game {
         playerLevel  = undefined,
         goalCoords   = undefined,
         steps        = undefined,
-        testImageP   = p1,
-        testImageG   = p2,
+        testImageP   = playerIcon,
+        testImageG   = goalIcon,
         seconds      = undefined
     }
+  
 
+{- main
+  Initiates the game
+  RETURNS: Gloss game
+-}
 main :: IO ()
 main = do
-    --Loads pictures
-    p1 <- loadBMP "2.bmp"
-    p2 <- loadBMP "2.bmp"
-    play window background fps (initialState p1 p2) render handleKeys counter
+    playerIcon <- loadBMP "2.bmp"
+    goalIcon   <- loadBMP "2.bmp"
+    
+    play 
+      window 
+      background 
+      30 --fps
+      (initialState playerIcon goalIcon) 
+      render 
+      handleKeys 
+      counter
+
+
+
+-- test1 :: Test
+test1 = TestCase $ assertEqual "Silly" (length "hej" == 3) True
+
+-- test2 :: Test
+test2 = TestCase $ assertEqual "Also silly" True True
+
+-- test3 :: Test
+test3 = TestCase $ assertEqual "Also silly" True True
+
+-- test4 :: Test
+test4 = TestCase $ assertEqual "Also silly" True True
+
+-- test5 :: Test
+test5 = TestCase $ assertEqual "Also silly" True True
+
+-- test6 :: Test
+test6 = TestCase $ assertEqual "Wow javla" True True
+
+-- runtests = runTestTT [test1,test2,test3,test4,test5,test6]
+
+-- runtests = runTestTT $ TestList [
+--     TestLabel "test1" test1, 
+--     TestLabel "test2" test2,
+--     TestLabel "test3" test3
+--   ]
--- Code/Delivery/Render.hs
+++ Code/Delivery/Render.hs
@@ -2,48 +2,39 @@ module Render where
 
 import Graphs
 
+import GameData
+
 import Graphics.Gloss
 
+{- The size of the Gloss window -}
+windowSize = 1000 :: Float
 
-windowSize, xMax, x0, yMax, y0 :: Float
-windowSize = 1000
-xMax = windowSize / 2
-x0 = negate $ windowSize / 2
-y0 = windowSize / 2
-yMax = negate $ windowSize / 2
+{-Information about Gloss Window | Window title ()-}
+window = InWindow "A Maz(e)ing game" (round windowSize + 200, round windowSize + 200) (10, 10) :: Display
 
-window :: Display
-window = InWindow "Rubrik" (round windowSize + 200, round windowSize + 200) (10, 10)
+{- Background colour -}
+background = white :: Color
 
-background :: Color
-background = white
+{- X-coordinate of the cell in the upper left corner -}
+x0 = negate $ windowSize / 2 :: Float
 
-fps :: Int
-fps = 30
+{- Y-coordinate of the cell in the upper left corner -}
+y0 = windowSize / 2 :: Float
 
+{- X-coordinate of the cell in the lower right corner -}
+xMax = windowSize / 2 :: Float
 
-{-GameState 
-  Represents the 
+{- Y-coordinate of the cell in the lower right corner -}
+yMax = negate $ windowSize / 2 :: Float
 
-
-  INVARIANT: gridSize > 0
+{- createMaze size
+  Creates the walls of a maze.
+    PRE: size > 0
+    RETURNS: 
+    EXAMPLES:
 -}
-data GameState = Game {
-        startMenu    :: Bool,
-        goalMenu     :: Bool,
-        gridSize     :: Float,
-        mazePicture  :: Picture,
-        walls        :: Maze,
-        playerCoords :: (Float, Float),
-        playerLevel  :: Int,
-        goalCoords   :: (Float, Float),
-        steps        :: Int,
-        testImageP   :: Picture,
-        testImageG   :: Picture,
-        seconds      :: Float
-    }
-
-
+createMaze :: Float -> Maze
+createMaze gridSize = Graphs.getMaze gridSize
 
 
 {-wallPicture maze 
@@ -56,61 +47,57 @@ wallPicture wall gridSize = Pictures (drawWalls wall gridSize)
 
 
 {-drawWalls maze size 
-Draws the walls of a maze.
+  Draws the walls of a maze.
     PRE: size > 0
-    RETURNS:
-    EXAMPLES:
+    RETURNS: Pictures of walls in "maze"
 -}
 drawWalls :: Maze -> Float -> [Picture]
 drawWalls [] _ = []
--- drawWalls lst@(x:xs) gridSize = foldl (\x -> singleWall x gridSize) Blank lst
-drawWalls (x:xs) gridSize =
-    singleWall x gridSize : drawWalls xs gridSize
-        where
-            singleWall :: Wall -> Float -> Picture
-            singleWall ((c1_x, c1_y), (c2_x, c2_y)) gridSize =
-                Line [(l1_x, l1_y), (l2_x, l2_y)]
-                where
-                    wallLength = windowSize / gridSize
-                    x_mid = x0 + wallLength * max c1_x c2_x
-                    y_mid = y0 - wallLength * max c1_y c2_y
-                    (l1_x, l2_x, l1_y, l2_y) =
-                        if (c1_x == c2_x) then
-                            (x_mid, x_mid + wallLength, y_mid, y_mid)
-                        else
-                            (x_mid, x_mid, y_mid, y_mid - wallLength)
+drawWalls lst size = map (singleWall size) lst
+    where
+        {- singleWall size wallCoord 
+             Draws the wall between two coordinates.
+               PRE: size > 0
+               RETURNS: Picture of a wall with length based on "size" laying between coordinates in "wallCoord".
+        -}
+        singleWall :: Float -> Wall -> Picture
+        singleWall gridSize ((c1_x, c1_y), (c2_x, c2_y)) =
+            Line [(l1_x, l1_y), (l2_x, l2_y)]
+            where
+                wallLength = windowSize / gridSize
+                x_mid = x0 + wallLength * max c1_x c2_x
+                y_mid = y0 - wallLength * max c1_y c2_y
+                (l1_x, l2_x, l1_y, l2_y) =
+                    if (c1_x == c2_x) 
+                        then (x_mid, x_mid + wallLength, y_mid, y_mid)
+                    else (x_mid, x_mid, y_mid, y_mid - wallLength)
 
-{- createMaze size
-  Creates the walls of a maze.
-    PRE: size > 0
-    RETURNS: 
-    EXAMPLES:
+
+{-translateCoordinates cell gridSize
+    Translates from cartesian coordinates to Gloss Window coordinates. 
+      PRE: gridSize > 0
+      RETURNS: The coordinates of "cell" in the gloss window.
+      EXAMPLES: 
+
+        translateCoordinates (0, 0) 25 == (-480.0, 480.0)
+        
+        translateCoordinates (1, 2) 20 == (-425.0, 375.0)
+
 -}
-createMaze :: Float -> Maze
-createMaze gridSize = Graphs.getMaze gridSize
-
-
-{-gridCalculate
-  Jag förstår inte den här geometrin, kan någon pls tala om för mig
-    PRE:
-    RETURNS:
-    EXAMPLES:
--}
-gridCalculate :: (Float, Float) -> Float -> (Float, Float)
-gridCalculate (x, y) gs = 
-    let wallLength = windowSize / gs
+translateCoordinates :: (Float, Float) -> Float -> (Float, Float)
+translateCoordinates (x, y) gridSize = 
+    let wallLength = windowSize / gridSize
     in (x0 + (x + 0.5) * wallLength, y0 - (y + 0.5) * wallLength)
 
 
-
 {-outerEdge size
-  Creates picture of the outer edge of a maze.
-    PRE: n >= 0
+  Creates picture of the outer edge of a grid.
+    PRE: size > 0
     RETURNS: A square whose side has length "size".
 -}
 outerEdge :: Float -> Picture
-outerEdge gs =
-    let wallLength = windowSize / gs in
+outerEdge gridSize =
+    let wallLength = windowSize / gridSize in
     pictures [
         Line [(x0, y0), (xMax, y0)],
         Line [(xMax, y0), (xMax, yMax + wallLength)],
@@ -118,36 +105,45 @@ outerEdge gs =
         Line [(x0, y0 - wallLength), (x0, yMax)]
     ]
 
+
 {-render state
-  Creates the picture of a game state
+  Creates the picture of a game state.
     RETURNS: The picture of the maze or menu which 'state' represents.
+
 -}
 render :: GameState -> Picture
 render game
     | startMenu game =
         pictures [
-            --scale 0.2 0.2 $ (testImage game),
             translate (-350) 100 $ scale 0.4 0.4 $ Text "Lets play a mazeing game!",
+            
             translate (-250) 0 $ scale 0.2 0.2 $ Text "Press [space] to never sleep again"
         ]
 
     | goalMenu game = 
         pictures [
             translate (-250) 80 $ scale 0.4 0.4 $ Text ("You passed level " ++ show (playerLevel game) ++ "!"),
-            translate (-320) 0 $ scale 0.3 0.3 $ Text ("With " ++ show (steps game) ++ " moves in " ++ (show (round (seconds game))) ++ " seconds"),
+            
+            translate (-320) 0 $ scale 0.3 0.3 $ Text ("With " ++ show (steps game) ++ " moves in " ++ show (round (seconds game)) ++ " seconds"),
+            
             translate (-250) (-60) $ scale 0.2 0.2 $ Text "Press [space] to go to the next level"
         ]
-            --translate (-250) (-60) $ scale 0.4 0.4 $ Text ("Time: "++(show (showTimeDiff  (timeNow game) (startTime game))))
-        
+            
     | otherwise = 
         pictures [
             outerEdge (gridSize game),
+            
             mazePicture game,
+            
             translate (-150) (y0 + 10) $ scale 0.4 0.4 $ Text ("Time: " ++ show (round (seconds game))),
-            uncurry translate (gridCalculate (goalCoords game) (gridSize game)) $ scale (0.2 * (gridSize game)) (0.2 * (gridSize game)) $ (testImageG game),--color green $ circleSolid ((windowSize / gridSize game) / 2 * 0.6),
-            uncurry translate (gridCalculate (playerCoords game) (gridSize game)) $  scale 0.5 0.5 $ (testImageP game),
-            translate x0 (y0 + 10) $ scale 0.4 0.4 $ Text ("Steps: "++show (steps game)),
-            translate 300 (y0 + 10) $ scale 0.4 0.4 $ Text ("Level: "++show (playerLevel game))
+            
+            uncurry translate (translateCoordinates (goalCoords game) (gridSize game)) $ scale (0.2 * (gridSize game)) (0.2 * gridSize game) $ testImageG game,
+            
+            uncurry translate (translateCoordinates (playerCoords game) (gridSize game)) $ scale 0.5 0.5 $ testImageP game,
+            
+            translate x0 (y0 + 10) $ scale 0.4 0.4 $ Text ("Steps: " ++show (steps game)),
+            
+            translate 300 (y0 + 10) $ scale 0.4 0.4 $ Text ("Level: " ++show (playerLevel game))
         ]
 
 
